3cd2dd81729c8d6751998056b59827a0ff08164e
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-07-13 16:27:22 -0400

    [TOKN-13] Implement less structure and live compile

--- src/components/Page404/index.js
+++ src/components/Page404/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Page404 = () => {
-  return <div className="404">404 Not Found</div>
+  return <div className="not-found">404 Not Found</div>
 }
 
 export default Page404
