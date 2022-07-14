daab0fed4923b29f85c1dcd83c528f4ab766db47
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-07-14 19:15:27 -0400

    [improvement] Separate exported less files from SD by category

--- style_dictionary/build.js
+++ style_dictionary/build.js
@@ -1,5 +1,7 @@
+const config = require('./config')
+
 //Create Style Dictionary by extending a configuration file
-const StyleDictionary = require('style-dictionary').extend('./style_dictionary/config.json')
+const StyleDictionary = require('style-dictionary').extend(config)
 
 //Import custom transforms
 const sizes = require('./custom/transforms/sizes')
@@ -9,19 +11,21 @@ const fonts = require('./custom/transforms/fonts')
 //Register transforms
 const transforms = [sizes, spacings, fonts]
 
-transforms.map( val => {
-    StyleDictionary.registerTransform({
-        name: val.name,
-        type: val.type,
-        matcher: val.matcher,
-        transformer: val.transformer
-    })
+transforms.map((val) => {
+  StyleDictionary.registerTransform({
+    name: val.name,
+    type: val.type,
+    matcher: val.matcher,
+    transformer: val.transformer
+  })
 })
 
 //Update Less transformGroup
 StyleDictionary.registerTransformGroup({
-    name: 'less',
-    transforms: StyleDictionary.transformGroup["less"].concat(transforms.map( val => val.name))
+  name: 'less',
+  transforms: StyleDictionary.transformGroup['less'].concat(
+    transforms.map((val) => val.name)
+  )
 })
 
 //This function will run once we execute npm run build script and will build the design files for every chosen platform at configuration file
---
+++ style_dictionary/config.js
@@ -0,0 +1,29 @@
+const tokens = require('../tokens/design-tokens.tokens.json')
+
+const tokensArray = []
+for (x in tokens) {
+  tokensArray.push({ [x]: tokens[x] })
+}
+
+module.exports = {
+  source: ['tokens/*.tokens.json'],
+  platforms: {
+    less: {
+      transformGroup: ['less'],
+      buildPath: 'less/_tokens/',
+      files: tokensArray.map((token) => {
+        const tokenCategory = Object.keys(token)[0]
+
+        return {
+          destination: `_${tokenCategory}.less`,
+          format: 'less/variables',
+          filter: {
+            attributes: {
+              category: tokenCategory
+            }
+          }
+        }
+      })
+    }
+  }
+}
