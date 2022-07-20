68a92dae613ddf4acfebdba11a1bd35c9002269e
Juan Pablo Michelli <jmichelli@techmatesgroup.com>
2022-07-20 13:15:17 -0300

    added product information storage

---
+++ src/product-storage.js
@@ -0,0 +1,68 @@
+const products = {
+  'hot-n-spicy': [
+    {
+      name: 'Clockwork Orange',
+      price: 4.99,
+      nutrition: 'high protein',
+      allergy: ['milk', 'eggs']
+    },
+    {
+      name: 'Red Planet',
+      price: 9.99,
+      nutrition: 'No Sugar',
+      allergy: null
+    },
+    {
+      name: 'Green Land',
+      price: 7.99,
+      nutrition: 'low fat',
+      allergy: ['milk', 'nuts', 'wheat']
+    },
+    {
+      name: 'Yellow Submarine',
+      price: 4.2,
+      nutrition: 'low fat',
+      allergy: ['fish']
+    },
+    {
+      name: 'Vivacious Violet',
+      price: 2.99,
+      nutrition: 'high protein',
+      allergy: null
+    }
+  ],
+  'cool-as-ice-colors': [
+    {
+      name: 'Pink Panther',
+      price: 4.99,
+      nutrition: 'low fat',
+      allergy: ['milk', 'eggs']
+    },
+    {
+      name: 'Feeling Blue',
+      price: 5.99,
+      nutrition: 'high protein',
+      allergy: ['milk', 'eggs', 'nuts']
+    },
+    {
+      name: 'Blue Clue',
+      price: 2.99,
+      nutrition: 'no sugar',
+      allergy: ['milk', 'nuts']
+    },
+    {
+      name: 'Gray Anatomy',
+      price: 6.9,
+      nutrition: 'high protein',
+      allergy: ['nuts']
+    },
+    {
+      name: 'Puzzled Plum',
+      price: 8.37,
+      nutrition: 'low fat',
+      allergy: ['wheat']
+    }
+  ]
+}
+
+export default products
