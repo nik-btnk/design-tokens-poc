e56bc9e6dd881836ecee60c01f027bef3d90111c
Juan Pablo Michelli <jmichelli@techmatesgroup.com>
2022-07-20 13:27:20 -0300

    added description field to ice cream objects

--- src/product-storage.js
+++ src/product-storage.js
@@ -2,30 +2,35 @@ const products = {
   'hot-n-spicy': [
     {
       name: 'Clockwork Orange',
+      description: '',
       price: 4.99,
       nutrition: 'high protein',
       allergy: ['milk', 'eggs']
     },
     {
       name: 'Red Planet',
+      description: '',
       price: 9.99,
       nutrition: 'No Sugar',
       allergy: null
     },
     {
       name: 'Green Land',
+      description: '',
       price: 7.99,
       nutrition: 'low fat',
       allergy: ['milk', 'nuts', 'wheat']
     },
     {
       name: 'Yellow Submarine',
+      description: '',
       price: 4.2,
       nutrition: 'low fat',
       allergy: ['fish']
     },
     {
       name: 'Vivacious Violet',
+      description: '',
       price: 2.99,
       nutrition: 'high protein',
       allergy: null
@@ -34,30 +39,35 @@ const products = {
   'cool-as-ice-colors': [
     {
       name: 'Pink Panther',
+      description: '',
       price: 4.99,
       nutrition: 'low fat',
       allergy: ['milk', 'eggs']
     },
     {
       name: 'Feeling Blue',
+      description: '',
       price: 5.99,
       nutrition: 'high protein',
       allergy: ['milk', 'eggs', 'nuts']
     },
     {
       name: 'Blue Clue',
+      description: '',
       price: 2.99,
       nutrition: 'no sugar',
       allergy: ['milk', 'nuts']
     },
     {
       name: 'Gray Anatomy',
+      description: '',
       price: 6.9,
       nutrition: 'high protein',
       allergy: ['nuts']
     },
     {
       name: 'Puzzled Plum',
+      description: '',
       price: 8.37,
       nutrition: 'low fat',
       allergy: ['wheat']
