2621c0f8d5fbccbe9d36318464494c2a7f8c2ef2
Juan Pablo Michelli <jmichelli@techmatesgroup.com>
2022-07-21 16:23:03 -0300

    Created test page, created basic button component

---
+++ src/pages/Test/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../../components/basics/Button'
+
+const Test = () => {
+  return (
+    <div className="test">
+      <h2>Test page</h2>
+      <Button />
+    </div>
+  )
+}
+
+export default Test
