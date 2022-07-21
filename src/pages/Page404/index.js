afde95b769a296e304c8dc91b3f90bb562f9a838
Juan Pablo Michelli <jmichelli@techmatesgroup.com>
2022-07-21 09:46:49 -0300

    Updated 404 page for all devices.Added abril font to project

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 const Page404 = () => {
   return (
-    <div className="not-found">
-      <div className="not-found__container">
-        <h2> 404 NOT FOUND </h2>
-        <h3> Sorry, we couldn&apos;t load this page :(</h3>
+    <div className="not-found-wrapper">
+      <div className="not-found">
+        <div className="not-found__logo">
+          <span>4</span>
+          <img src={require('../../assets/404-mascot.png')}></img>
+          <span>4</span>
+        </div>
+        <div className="not-found__text1">
+          <p>
+            Sorry, we couldn’t find the <br></br>page you’re looking for!
+          </p>
+        </div>
+        <div className="not-found__text2">
+          <p>
+            <b>Maybe spin the Wheel O’ Color and find a new favorite?</b>
+          </p>
+        </div>
       </div>
     </div>
   )
