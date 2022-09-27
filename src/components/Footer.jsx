e5384f8beb21c2054e0532c3a548329d6d363a5b
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-09-27 16:54:32 -0400

    [TOKN-69] Clean up and improve responsive behaviour of footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,26 @@ import iconIg from '../assets/carbon_logo-instagram.png'
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__contact-container">
-        <span className="footer__gmail">example@gmail.com</span>
-        <span className="footer__phone">+1 (999) 123-12-12</span>
+      <div className="footer__contact">
+        <span className="footer__contact--email">scoop@creamcolors.com</span>
+        <span className="footer__contact--phone">+1 (999) 123-1212</span>
       </div>
       <div className="footer__logo">
         <Link to="/">
-          <img src={mascot} alt="" />
+          <img src={mascot} alt="Cream Colors Logo." />
         </Link>
       </div>
-      <div className="footer__social-container">
-        <img className="footer__tiktok" src={iconTikTok} alt="" />
-        <img className="footer__instagram" src={iconIg} alt="" />
-      </div>
-      <div className="footer__copy">
-        <p>Design Token&apos;s Lonely Hearts Club Band &copy;2022</p>
+      <div className="footer__social">
+        <img
+          className="footer__social--tiktok"
+          src={iconTikTok}
+          alt="TikTok link icon."
+        />
+        <img
+          className="footer__social--instagram"
+          src={iconIg}
+          alt="Instagram link icon."
+        />
       </div>
     </footer>
   )
