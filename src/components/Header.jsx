e8c125bbb4ecd15c148d30fd898443e1b0d929c8
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-09-28 13:17:24 -0400

    [TOKN-69] Clean up header and add responsive behaviour (excludes desktop breakpoint)

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+//assets
+import menuBars from '../assets/Icon=menu-bars.png'
+import logo from '../assets/logo.png'
+import iconCart from '../assets/Icon=cart-menu.png'
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  useEffect(() => {
+    console.log(`Menu is ${menuOpen ? 'open' : 'closed'}.`)
+  }, [menuOpen])
+
+  return (
+    <header className="header">
+      <button
+        className="header__menu-button"
+        onClick={() => setMenuOpen((prev) => !prev)}>
+        <img src={menuBars} alt="Menu icon." />
+        <span>Menu</span>
+      </button>
+      <Link to="/" className="header__logo">
+        <img src={logo} alt="Cream Colors logo." />
+      </Link>
+      <Link to="/cart" className="header__cart">
+        <img src={iconCart} alt="Cart icon." />
+      </Link>
+    </header>
+  )
+}
+
+export default Header
