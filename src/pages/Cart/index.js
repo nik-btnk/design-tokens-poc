3edc6fa0573ba3d8d26fa54bff60eb196934ede9
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-10-05 13:02:18 -0400

    [TOKN-94] Create and implement product specific url routing.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,9 @@
+// Modules
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+// Utils
+import { path } from '../../constants'
 
 const Cart = () => {
   return (
@@ -7,10 +11,10 @@ const Cart = () => {
       <span className="cart__title">Cart Page</span>
 
       <div className="cart__nav">
-        <Link to="/">
+        <Link to={path.LANDING}>
           <button className="cart__nav--shop">To Landing</button>
         </Link>
-        <Link to="/productList">
+        <Link to={path.PRODUCTS}>
           <button className="cart__nav--productList">To Product List</button>
         </Link>
       </div>
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -2,16 +2,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// Utils
+import { path } from '../../constants'
+
 const Landing = () => {
   return (
     <div className="landing">
       <span className="landing__title">Landing Page</span>
 
       <div className="landing__nav">
-        <Link to="/productList">
+        <Link to={path.PRODUCTS}>
           <button className="landing__nav--productList">To Product List</button>
         </Link>
-        <Link to="/cart">
+        <Link to={path.CART}>
           <button className="landing__nav--cart">To Cart</button>
         </Link>
       </div>
