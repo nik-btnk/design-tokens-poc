b02db5254020f9a1de40d3431d1393f3aedb4607
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-11-08 16:55:11 -0500

    Improve example implementation

--- src/pages/Playground/index.js
+++ src/pages/Playground/index.js
@@ -15,6 +15,19 @@ const Playground = () => {
         }}>
         Toggle Theme
       </button>
+
+      <div className="playground__content">
+        <h1 className="playground__content--title">PAGE CONTENT</h1>
+        <p className="playground__content--article">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </div>
   )
 }
