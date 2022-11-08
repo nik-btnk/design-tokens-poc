ebae449ae3585728e946249ebdc0dd82c63af119
Nikolay Butnik <nbutnik@techmatesgroup.com>
2022-11-08 16:30:51 -0500

    Implement proof of concept

--- src/pages/Playground/index.js
+++ src/pages/Playground/index.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Playground = () => {
-  return <div className="playground">Playground</div>
+  const [theme, setTheme] = useState('light-theme')
+
+  return (
+    <div className={`playground ${theme}`}>
+      <button
+        onClick={() => {
+          if (theme === 'light-theme') {
+            setTheme('dark-theme')
+          } else {
+            setTheme('light-theme')
+          }
+        }}>
+        Toggle Theme
+      </button>
+    </div>
+  )
 }
 
 export default Playground
