1a9c57ad2e59a174f91df9ae37cdd7d6ef8b76d2
Juan Pablo Michelli <jmichelli@techmatesgroup.com>
2022-11-30 18:24:50 -0300

    fix update

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -17,29 +17,31 @@ const Cart = () => {
 
   return (
     <div className="cart">
-      <OrderSummary></OrderSummary>
-      <section className="cart__shopping-cards">
-        {/* Shoppping Cards would be rendered dynamically */}
-        {selectedProducts.length > 0 ? (
-          selectedProducts.map((product) => (
-            <ShoppingCard
-              product={product}
-              key={product.id}
-              productTotal={calcProductTotal(product.id)}
-            />
-          ))
-        ) : (
-          <h2 style={{ textAlign: 'center' }}>No products on cart</h2>
-        )}
-      </section>
-      <div className="cart__total-price">
-        <span className="cart__total-price--text">Total Price</span>
-        <span className="cart__total-price--price">
-          ${productsTotal.toFixed(2)}
-        </span>
-      </div>
-      <div className="cart__continue-checkout">
-        <button>Continue to Checkout</button>
+      <div className="cart__wrapper">
+        <OrderSummary></OrderSummary>
+        <section className="cart__shopping-cards">
+          {/* Shoppping Cards would be rendered dynamically */}
+          {selectedProducts.length > 0 ? (
+            selectedProducts.map((product) => (
+              <ShoppingCard
+                product={product}
+                key={product.id}
+                productTotal={calcProductTotal(product.id)}
+              />
+            ))
+          ) : (
+            <h2 style={{ textAlign: 'center' }}>No products on cart</h2>
+          )}
+        </section>
+        <div className="cart__total-price">
+          <span className="cart__total-price--text">Total Price</span>
+          <span className="cart__total-price--price">
+            ${productsTotal.toFixed(2)}
+          </span>
+        </div>
+        <div className="cart__continue-checkout">
+          <button>Continue to Checkout</button>
+        </div>
       </div>
     </div>
   )
