23103d766e129217d6009ccc93f199766460b375
Juan Pablo Michelli <jmichelli@techmatesgroup.com>
2022-12-05 11:07:10 -0300

    Implemented 'added' animation

--- src/components/cart/CartButton.jsx
+++ src/components/cart/CartButton.jsx
@@ -1,37 +1,48 @@
 import React, { useState } from 'react'
 import { useContext } from 'react'
+import CartContext from '../../contexts/CartContext/CartProvider'
+import QuantitySelector from './QuantitySelector'
 
 //Assets
 import { ReactComponent as IconCart } from '../../assets/Icon=cart-add.svg'
 import { ReactComponent as IconCheck } from '../../assets/icons/status/Icon=check.svg'
-import CartContext from '../../contexts/CartContext/CartProvider'
-import QuantitySelector from './QuantitySelector'
 
 const AddToCartBtn = ({ id }) => {
-  const { addProduct, isAdded, selectedProducts } = useContext(CartContext)
-  const { isAddedVisible, setIsAddedVisible } = useState(true)
+  const { addProduct, selectedProducts } = useContext(CartContext)
   const myProduct = selectedProducts.find((product) => product.id == id)
+  const [isAdded, setIsAdded] = useState(myProduct?.quantity > 0)
+  const [btnDisplay, setBtnDisplay] = useState(false)
   const handleClick = () => {
     addProduct(id)
-    if (5 < selectedProducts.length + 6) setIsAddedVisible(true)
-  }
+    setBtnDisplay((prev) => !prev)
 
+    setTimeout(() => {
+      setIsAdded(true)
+    }, 1200)
+  }
   return (
     <>
-      {isAddedVisible ? (
-        <button className="add-to-cart-btn">
-          <IconCheck />
-          Added
-        </button>
-      ) : isAdded(id) ? (
+      {isAdded ? (
         <button className="add-to-cart-btn">
           <QuantitySelector id={id} quantity={myProduct.quantity} />
         </button>
       ) : (
-        <button className="add-to-cart-btn" onClick={handleClick}>
-          <IconCart />
-          Add to Cart
-        </button>
+        <>
+          <button
+            className="add-to-cart-btn added"
+            style={btnDisplay ? { display: 'flex' } : { display: 'none' }}>
+            <IconCheck />
+            <span>Added</span>
+          </button>
+
+          <button
+            className="add-to-cart-btn"
+            onClick={handleClick}
+            style={btnDisplay ? { display: 'none' } : { display: 'flex' }}>
+            <IconCart />
+            Add to Cart
+          </button>
+        </>
       )}
     </>
   )
